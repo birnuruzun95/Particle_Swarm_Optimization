80e651eaf10922b6841833c58500d096875b45c8
birnuruzun95 <birnuruzun.294976@gmail.com>
2017-05-05 01:17:47 +0300

    changed

--- bitirme.py
+++ bitirme.py
@@ -10,26 +10,30 @@ konum = [[0,0,0,0,0,0,0],
          [0,2,0,2,0,2,0],
          [0,0,0,0,0,0,0]]
 
-x = random.randint(0,1)
-y = random.randint(0,1)
+oda = 9
+t_alan = 49
+yangin = 0
 
-def yangin(x,y):
-    
-   if konum[x][y]==2:
-      print("ROOM") 
-   elif konum[x][y]==0: 
-       
-      deger = random.randint(0,1)  
-      konum[x][y] = deger
+rastgele_yangin_sayisi = t_alan - oda - yangin
 
-   else:
-      print("Fire!")
-    
-    #for i in range(0,7):
-	   #for j in range(random.randint(0,2)):
-	      # eleman+=[j]
-   for i in range (0,7):
-     print(konum[i])
+def yangin_olusturucu():
+   global yangin
+   while(yangin < rastgele_yangin_sayisi/5):
+	x = random.randint(0,6)
+	y = random.randint(0,6)
+	if konum[x][y]!=2:
+	   konum[x][y]=1
+	   yangin+=1
+	else:
+	   continue 
 
-yangin(x,y)
+yangin_olusturucu()
+
+for i in range (0,7):
+   print(konum[i])
+  
+#for i in range(0,7):
+ #for j in range(random.randint(0,2)):
+    # eleman+=[j]
+
 
