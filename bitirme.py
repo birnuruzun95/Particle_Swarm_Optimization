632790181c0ee0399730d9073d05e03ca355b35d
birnuruzun95 <birnuruzun.294976@gmail.com>
2017-05-04 18:34:53 +0300

    changed

--- bitirme.py
+++ bitirme.py
@@ -3,27 +3,33 @@
 import random
 
 konum = [[0,0,0,0,0,0,0],
+	 [0,2,0,2,0,2,0],
 	 [0,0,0,0,0,0,0],
-	 [0,0,0,0,0,0,0],
+         [0,2,0,2,0,2,0],
          [0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0],
+         [0,2,0,2,0,2,0],
          [0,0,0,0,0,0,0]]
-x = input("x:")
-y = input("y:")
+
+x = random.randint(0,1)
+y = random.randint(0,1)
 
 def yangin(x,y):
     
-    eleman = []
-    for i in range(0,7):
-	   for j in range(random.randint(0,2)):
-	       eleman+=[j]
-    if eleman == 1:
-	konum[x][y] = eleman
-    else:
-	konum[x][y] = 0
-    print(konum)
-	   
+   if konum[x][y]==2:
+      print("ROOM") 
+   elif konum[x][y]==0: 
        
-print(yangin(x,y))
+      deger = random.randint(0,1)  
+      konum[x][y] = deger
+
+   else:
+      print("Fire!")
+    
+    #for i in range(0,7):
+	   #for j in range(random.randint(0,2)):
+	      # eleman+=[j]
+   for i in range (0,7):
+     print(konum[i])
+
+yangin(x,y)
 
