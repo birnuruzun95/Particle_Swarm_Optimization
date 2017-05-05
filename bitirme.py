36ffd4356c8b0f53b537a56916740bb83eb43c07
birnuruzun95 <birnuruzun.294976@gmail.com>
2017-05-06 02:40:00 +0300

    changed

--- bitirme.py
+++ bitirme.py
@@ -9,42 +9,137 @@ konum = [[0,1,0,0,0,0,0],
          [0,1,0,0,0,0,0],
          [0,2,1,2,0,2,0],
          [0,0,0,0,0,1,0]]
+def matris_yazdir():
+    for i in range (0,7):
+        print(konum[i])
+matris_yazdir()
+#-------------------------------------------------------------------#
 
-#for i in range (0,7):
-#   print(konum[i])
-
-yangin_ym = []
+yangin_ym = [[0,0,0],
+	     [0,0,0],
+             [0,0,0],
+             [0,0,0]]
 
 def yangin_yatay():
     global yangin_ym
-    for x in range(0,7):
-       if x%2==0:
-	  for y in range(0,7):
-    	      if y%2==0:
-	          y+=1
-	          continue
-       	      elif y%2!=0:
-	          yangin_ym = konum[x][y]
-	          print(yangin_ym)
-                  
-       else:
-         x+=1
+    i = 0
+    j = 0
+  
+    print("yatay yangin matrisi:\n")
+    for x in range(0,len(konum),2):	
+	for y in range(1,len(konum[0]),2):
+            yangin_ym[i][j] = konum[x][y]
+            j += 1
+	i += 1
+	j = 0
+    for k in range (0,len(yangin_ym)):
+        print(yangin_ym[k])
+
+print("\n")
+yangin_yatay()
+#------------------------------------------------------------------#
+
+yangin_dm = [[0,0,0,0],
+             [0,0,0,0],
+             [0,0,0,0]]
 
 def yangin_dikey():
-    global yangin_ym
-    for x in range(0,7):
-       if x%2!=0:
-	  for y in range(0,7):
-    	      if y%2!=0:
-	          y+=1
-	          continue
-       	      elif y%2==0:
-	          yangin_ym = konum[x][y]
-	          print(yangin_ym)
-                  
-       else:
-         x+=1
+    global yangin_dm
+    i = 0
+    j = 0
+    print("dikey yangın matrisi:\n")
+    for x in range(1,len(konum),2):
+	for y in range(0,len(konum[0]),2):
+	    yangin_dm[i][j] = konum[x][y]
+            j += 1
+	i += 1
+	j = 0
+    for k in range (0,len(yangin_dm)):
+        print(yangin_dm[k])
+
 print("\n")
 yangin_dikey()
 print("\n")
-yangin_dikey()
+#------------------------------------------------------------------#
+
+scdm_ym = [[0,0,0],
+           [0,0,0],
+           [0,0,0],
+	   [0,0,0]]
+
+def sicaklik_duman_yatay():
+    global scdm_ym
+    for x in range(0,len(scdm_ym)):
+	for y in range(0,len(scdm_ym[0])):
+	    if(yangin_ym[x][y] == 1): 
+		if (y-1 >= 0):
+		   scdm_ym[x][y-1]+=0.25
+		if (y+1 <= len(scdm_ym[0])-1):
+		   scdm_ym[x][y+1]+=0.25
+		if (x-1 >= 0):
+		   scdm_ym[x-1][y]+=0.25
+		if (x+1 <= len(scdm_ym)-1):
+                   scdm_ym[x+1][y]+=0.25
+    for k in range (0,len(scdm_ym)):
+        print(scdm_ym[k])
+
+print("yatay sicaklik/duman matrisi:\n")
+sicaklik_duman_yatay()
+print("\n")
+#------------------------------------------------------------------#
+		
+scdm_dm = [[0,0,0,0],
+	   [0,0,0,0],
+	   [0,0,0,0]]
+
+def sicaklik_duman_dikey():
+    global scdm_dm
+    for x in range(0,len(scdm_dm)):
+	for y in range(0,len(scdm_dm[0])):
+	    if(yangin_dm[x][y] == 1): 
+		if (y-1 >= 0):
+		   scdm_dm[x][y-1]+=0.25
+		if (y+1 <= len(scdm_dm[0])-1):
+		   scdm_dm[x][y+1]+=0.25
+		if (x-1 >= 0):
+		   scdm_dm[x-1][y]+=0.25
+		if (x+1 <= len(scdm_dm)-1):
+                   scdm_dm[x+1][y]+=0.25
+    for k in range (0,len(scdm_dm)):
+        print(scdm_dm[k])
+
+print("dikey sicaklik/duman matrisi:\n")
+sicaklik_duman_dikey()
+print("\n")
+#------------------------------------------------------------------#	
+
+isik_ym = [[1,1,1],
+	   [0,0,0],
+	   [0,0,0],
+	   [1,1,1]]
+
+print("yatay ışık matrisi:\n")
+for k in range (0,len(isik_ym)):
+    print(isik_ym[k])
+print("\n")
+#------------------------------------------------------------------#
+
+isik_dm = [[1,0,0,1],
+	   [1,0,0,1],
+	   [1,0,0,1]]
+
+print("dikey ışık matrisi:\n")
+for k in range (0,len(isik_dm)):
+    print(isik_dm[k])
+print("\n")
+#------------------------------------------------------------------#
+
+
+
+
+
+
+
+
+
+
