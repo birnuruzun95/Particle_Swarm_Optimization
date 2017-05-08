f1073c8d631e9e9f5cebeb0505bba1ad014c3154
birnuruzun95 <birnuruzun.294976@gmail.com>
2017-05-09 02:08:38 +0300

    changed

--- matrix_and_neo.py
+++ matrix_and_neo.py
@@ -7,8 +7,8 @@ def matris_yazdir(matris):
        print(matris[i])
 
 konum = [[0,1,0,0,0,0,0],
-	 [0,2,1,2,0,2,0],
-	 [0,0,0,1,0,0,0],
+	     [0,2,1,2,0,2,0],
+	     [0,0,0,1,0,0,0],
          [0,2,0,2,0,2,1],
          [0,1,0,0,0,0,0],
          [0,2,1,2,0,2,0],
@@ -18,7 +18,7 @@ matris_yazdir(konum)
 #-------------------------------------------------------------------#
 
 yangin_ym = [[0,0,0],
-	     [0,0,0],
+	         [0,0,0],
              [0,0,0],
              [0,0,0]]
 
@@ -29,12 +29,12 @@ def yangin_yatay():
   
     print("yatay yangin matrisi:\n")
     for x in range(0,len(konum),2):	
-	for y in range(1,len(konum[0]),2):
+        for y in range(1,len(konum[0]),2):
             yangin_ym[i][j] = konum[x][y]
             j += 1
-	i += 1
-	j = 0
-    matris_yazdir(yangin_ym)    
+        i += 1
+        j = 0
+    matris_yazdir(yangin_ym)
 print("\n")
 yangin_yatay()
 #------------------------------------------------------------------#
@@ -49,11 +49,11 @@ def yangin_dikey():
     j = 0
     print("dikey yangın matrisi:\n")
     for x in range(1,len(konum),2):
-	for y in range(0,len(konum[0]),2):
-	    yangin_dm[i][j] = konum[x][y]
+        for y in range(0,len(konum[0]),2):
+            yangin_dm[i][j] = konum[x][y]
             j += 1
-	i += 1
-	j = 0
+        i += 1
+        j = 0
     matris_yazdir(yangin_dm) 
 
 print("\n")
@@ -64,21 +64,21 @@ print("\n")
 scdm_ym = [[0,0,0],
            [0,0,0],
            [0,0,0],
-	   [0,0,0]]
+	       [0,0,0]]
 
 def sicaklik_duman_yatay():
     global scdm_ym
     for x in range(0,len(scdm_ym)):
-	for y in range(0,len(scdm_ym[0])):
-	    if(yangin_ym[x][y] == 1): 
-		if (y-1 >= 0):
-		   scdm_ym[x][y-1]+=0.25
-		if (y+1 <= len(scdm_ym[0])-1):
-		   scdm_ym[x][y+1]+=0.25
-		if (x-1 >= 0):
-		   scdm_ym[x-1][y]+=0.25
-		if (x+1 <= len(scdm_ym)-1):
-                   scdm_ym[x+1][y]+=0.25
+        for y in range(0,len(scdm_ym[0])):
+            if(yangin_ym[x][y] == 1):
+                if (y-1 >= 0):
+                    scdm_ym[x][y-1]+=0.25
+                if (y+1 <= len(scdm_ym[0])-1):
+                    scdm_ym[x][y+1]+=0.25
+                if (x-1 >= 0):
+                    scdm_ym[x-1][y]+=0.25
+                if (x+1 <= len(scdm_ym)-1):
+                    scdm_ym[x+1][y]+=0.25
     matris_yazdir(scdm_ym) 
 
 print("yatay sicaklik/duman matrisi:\n")
@@ -87,22 +87,22 @@ print("\n")
 #------------------------------------------------------------------#
 		
 scdm_dm = [[0,0,0,0],
-	   [0,0,0,0],
-	   [0,0,0,0]]
+	       [0,0,0,0],
+	       [0,0,0,0]]
 
 def sicaklik_duman_dikey():
     global scdm_dm
     for x in range(0,len(scdm_dm)):
-	for y in range(0,len(scdm_dm[0])):
-	    if(yangin_dm[x][y] == 1): 
-		if (y-1 >= 0):
-		   scdm_dm[x][y-1]+=0.25
-		if (y+1 <= len(scdm_dm[0])-1):
-		   scdm_dm[x][y+1]+=0.25
-		if (x-1 >= 0):
-		   scdm_dm[x-1][y]+=0.25
-		if (x+1 <= len(scdm_dm)-1):
-                   scdm_dm[x+1][y]+=0.25
+        for y in range(0,len(scdm_dm[0])):
+            if(yangin_dm[x][y] == 1):
+                if (y-1 >= 0):
+                   scdm_dm[x][y-1]+=0.25
+                if (y+1 <= len(scdm_dm[0])-1):
+                   scdm_dm[x][y+1]+=0.25
+                if (x-1 >= 0):
+                   scdm_dm[x-1][y]+=0.25
+                if (x+1 <= len(scdm_dm)-1):
+                           scdm_dm[x+1][y]+=0.25
     matris_yazdir(scdm_dm) 
 
 print("dikey sicaklik/duman matrisi:\n")
@@ -111,9 +111,9 @@ print("\n")
 #------------------------------------------------------------------#	
 
 isik_ym = [[1,1,1],
-	   [0,0,0],
-	   [0,0,0],
-	   [1,1,1]]
+	       [0,0,0],
+	       [0,0,0],
+	       [1,1,1]]
 
 print("yatay ışık matrisi:\n")
 matris_yazdir(isik_ym)
@@ -121,8 +121,8 @@ print("\n")
 #------------------------------------------------------------------#
 
 isik_dm = [[1,0,0,1],
-	   [1,0,0,1],
-	   [1,0,0,1]]
+	       [1,0,0,1],
+	       [1,0,0,1]]
 
 print("dikey ışık matrisi:\n")
 matris_yazdir(isik_dm) 
@@ -134,13 +134,13 @@ mesafe = 13
 result_ym = [[0,0,0],
              [0,0,0],
              [0,0,0],
-	     [0,0,0]]
+	         [0,0,0]]
 
 def result_yatay():
     global result_ym
     for x in range(0,len(result_ym)):
         for y in range(0,len(result_ym[0])):
-	    result_ym[x][y] = float(2*mesafe + (scdm_ym[x][y]/mesafe) - (1.5*isik_ym[x][y]))
+            result_ym[x][y] = float(2*mesafe + (scdm_ym[x][y]/mesafe) - (1.5*isik_ym[x][y]))
 
 result_yatay()
 print("yatay ara sonuç  matrisi:\n")
@@ -149,15 +149,15 @@ print("\n")
 #------------------------------------------------------------------#
 
 result_dm = [[0,0,0,0],
-	     [0,0,0,0],
-	     [0,0,0,0]]
+	         [0,0,0,0],
+	         [0,0,0,0]]
 
 
 def result_dikey():
     global result_dm
     for x in range(0,len(result_dm)):
-	for y in range(0,len(result_dm[0])):
-	    result_dm[x][y] = float(2*mesafe + (scdm_dm[x][y]/mesafe) - (1.5*isik_dm[x][y]))
+        for y in range(0,len(result_dm[0])):
+            result_dm[x][y] = float(2*mesafe + (scdm_dm[x][y]/mesafe) - (1.5*isik_dm[x][y]))
 
 result_dikey()
 print("Dikey Ara Sonuç Matrisi:")
---
+++ pso.py
@@ -0,0 +1,115 @@
+import matrix_and_neo
+import random as rand
+import numpy as np
+import matplotlib.pyplot as plt
+
+def parcacik():
+    #---------------------degiskenler ve ilk deger atamaları----------------------
+
+    num_dimension = 2
+    num_parcacik = 15
+
+    p = np.empty((num_dimension, num_parcacik))
+    v = np.empty((num_dimension, num_parcacik))
+
+    pbest = np.empty((num_dimension, num_parcacik))
+    gbest = np.empty((1,2))
+    error = np.empty((num_parcacik))
+
+    for i in range(0,num_dimension):
+        for j in range(0,num_parcacik):
+            pbest[i][j] = rand.randint(-7,7)
+            p[i][j] = pbest[i][j]
+            v[i][j] = 0
+
+    #--------------anlik fitness fonksiyonunun hesaplanmasi-----------------------------------
+
+
+    for i in range(0,num_parcacik):
+        error[i] = fonksiyon(p[0][i], p[1][i])
+
+    karsilastirma(pbest, error, num_parcacik)
+
+    gbest[0][0] = pbest[0][0]
+    gbest[0][1] = pbest[0][1]
+
+    iterasyon = 0
+
+    plt.ion()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.grig(True)
+
+    while(iterasyon < 30):
+        for i in range(0,num_parcacik):
+            if (fonksiyon(p[0][i], p[1][i]))< (fonksiyon(pbest[0][i], pbest[1][i])):
+                pbest[0][i] = p[0][i]
+                pbest[1][i] = p[1][i]
+            if (fonksiyon(pbest[0][i], pbest[1][i]))< (fonksiyon(gbest[0][i], gbest[1][i])):
+                gbest[0][0] = p[0][i]
+                gbest[0][1] = p[1][i]
+
+            hiz_guncellemesi(num_parcacik, p, pbest, gbest, v)
+
+            iterasyon +=1
+
+            print ( "iterasyon: {} ---------- gbest:{} ".format(iterasyon, gbest))
+
+            line1 = ax.plot(p[0],p[1],'error+')
+            line2 = ax.plot(gbest[0][0], gbest[0][1], 'g*')
+
+            ax.set_xlim(-3,3)
+            ax.set_ylim(-3,3)
+
+            fig.canvas.draw()
+
+            ax.clear()
+            ax.grid(True)
+
+            print("gbest:{}".format(gbest))
+
+def hiz_guncellemesi(num_parcacik, p, pbest, gbest, v):
+
+    w = 0.7
+    c1 = 1.47
+    c2 = 1.47
+
+    r1 = rand.random()
+    r2 = rand.random()
+
+    for i in range(0, num_parcacik):
+        #--------- x ekseni icin hız guncellemesi
+        v[0][i] = w*v[0][i] + c1*r1*(pbest[0][i]- p[0][i]) +c2**r1*(gbest[0][0]-p[0][i])
+        p[0][i] = p[0][i] + v[0][i]
+        # -------- y ekseni icin hız guncellemesi
+        v[1][i] = w * v[1][i] + c1 * r1 * (pbest[1][i] - p[1][i]) + c2 ** r1 * (gbest[0][1] - p[1][i])
+        p[1][i] = p[1][i] + v[1][i]
+
+def fonksiyon(result_ym, result_dm):
+    matrix_and_neo.result_yatay()
+    matrix_and_neo.result_dikey()
+    return result_ym, result_dm
+
+def karsilastirma(pbest, error, num_parcacik):
+    for i in range (1, num_parcacik):
+        for j in range (0, num_parcacik-1):
+            if (error[j] > error[j+1]):
+                #fitness fonksiyonunu siralama
+                temp = error[j]
+                error[j] = error[j+1]
+                error[j+1] =temp
+
+                # en uygun pozisyonu siralama
+                tempx = pbest[0][j]
+                pbest[0][j] = pbest[0][j+1]
+                pbest[0][j+1] = tempx
+
+                tempy = pbest[1][j]
+                pbest[1][j] = pbest[1][j + 1]
+                pbest[1][j + 1] = tempy
+
+if __name__== '__main__':
+    parcacik()
+
+
+
