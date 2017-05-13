25781723e82661fc0fdeb3c85e6e11a9fb215600
birnuruzun95 <birnuruzun.294976@gmail.com>
2017-05-14 01:59:46 +0300

    changed

--- pso.py
+++ pso.py
@@ -1,4 +1,6 @@
-import matrix_and_neo
+# -*- coding: utf-8 -*-
+
+import matrix_and_neo as mn
 import random as rand
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,8 +30,8 @@ def parcacik():
             v[i][j] = 0
 
     for i in range(0, num_parcacik):
-        error[i] = func(p[0][i], p[1][i])
-
+        error[i] = funcy(p[0][i], p[1][i])
+        error[i] = funcd(p[0][i], p[1][i])
     uygunluk_fonk(pbest, error, num_parcacik)
 
     gbest[0][0] = pbest[0][0]
@@ -37,11 +39,19 @@ def parcacik():
 
     while iterasyon < 100 :
         for i in range(0, num_parcacik):
-            if (func(p[0][i], p[1][i])) < (func(pbest[0][i], pbest[1][i])):
+            if (funcy(p[0][i], p[1][i])) < (funcy(pbest[0][i], pbest[1][i])):
                 pbest[0][i] = p[0][i]
                 pbest[1][i] = p[1][i]
 
-            if (func(pbest[0][i], pbest[1][i])) < (func(gbest[0][0], gbest[0][1])):
+            if (funcy(pbest[0][i], pbest[1][i])) < (funcy(gbest[0][0], gbest[0][1])):
+                gbest[0][0] = p[0][i]
+                gbest[0][1] = p[1][i]
+
+            if (funcd(p[0][i], p[1][i])) < (funcd(pbest[0][i], pbest[1][i])):
+                pbest[0][i] = p[0][i]
+                pbest[1][i] = p[1][i]
+
+            if (funcd(pbest[0][i], pbest[1][i])) < (funcd(gbest[0][0], gbest[0][1])):
                 gbest[0][0] = p[0][i]
                 gbest[0][1] = p[1][i]
 
@@ -69,15 +79,23 @@ def parcacik():
     print("gbest:{}".format(gbest))
     print("Pbest:{}".format(pbest))
 
-def func(x,y):
-    matrix_and_neo.result_yatay()
-    matrix_and_neo.result_dikey()
-    resultyatay = matrix_and_neo.result_ym
-    resultdikey = matrix_and_neo.result_dm
+def funcy(x,y):
+    mn.result_yatay()
+    resultyatay = mn.result_ym
 
     for x in range(0, 4):
         for y in range(0, 3):
-            return resultyatay[x][y] * resultdikey[y][x]
+            return resultyatay[x][y]
+
+
+
+def funcd(x,y):
+    mn.result_dikey()
+    resultdikey = mn.result_dm
+
+    for x in range(0, 3):
+        for y in range(0, 4):
+            return resultdikey[x][y]
 
 def hiz_guncellemesi(num_parcacik, p, pbest, gbest, v):
         w = 0.7
